48b834ad9ab4537dbaef473dcec02daabf80c83c
mario.figallo@mail.uotoronto.ca <figallos@ug140.eecg.toronto.edu>
2024-01-22 17:56:50 -0500

    deleted unecessary

--- Lab1/Section1/deliver/deliver.c
+++ Lab1/Section1/deliver/deliver.c
@@ -11,27 +11,10 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s hostaddress port\n", argv[0]);
         return EXIT_FAILURE;
     }
-
-    struct addrinfo hints, *res;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_DGRAM;
-
-    // handle error
-    getaddrinfo(*argv[1], *argv[2], &hints, &res);
-    // res now points to a linked list of 1 or more struct addrinfos
-
-    // ... create socket and use res for the address in sendto
-
-    freeaddrinfo(res); // free the linked list when done
-
-    int getaddrinfo()
-
-
     
     int fd = socket(AF_INET, SOCK_DGRAM, AF_INET);
 
-    
+
 
     return EXIT_SUCCESS;
 }
