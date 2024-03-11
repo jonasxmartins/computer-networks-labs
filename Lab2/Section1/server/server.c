65ededeee190af648d332906c50302c8011323f3
mario.figallo@mail.uotoronto.ca <figallos@ug140.eecg.toronto.edu>
2024-03-11 02:30:44 -0400

    created folders and makefile for lab2

---
+++ Lab2/Section1/server/server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    return 0;
+}
