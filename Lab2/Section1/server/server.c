ef09228e185dc729887e9cd2380324575a96ea4c
Jonas Martins <jonas.martins@mail.utoronto.ca>
2024-03-11 21:58:24 -0400

    working on client_handler and thread argument parsing

--- Lab2/Section1/server/server.c
+++ Lab2/Section1/server/server.c
@@ -7,10 +7,23 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+struct thread_args {
+    int new_sock;
+    unsigned int *n_sessions;
+    unsigned int *n_clients;
+    struct Client *client_list;
+    struct Session *session_list;
+};
 
-void *client_handler(void *arg) {
-    int sock = *(int *)arg;
-    free(arg); // Free the heap memory allocated for the socket descriptor
+
+void *client_handler(void *args) {
+    int sock = *(int *)args[0];
+    unsigned int *n_sessions = args[1];
+    unsigned int *n_clients = args[2];
+    struct Client *client_list = args[3];
+    struct Session *session_list = (struct Session *) args[4];
+    
+    free(args); // Free the heap memory allocated for the socket descriptor
 
     // You can use a loop here to continuously read from the socket
     // and process commands from the client based on your protocol
@@ -50,6 +63,12 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // key variables for server functionality
+    unsigned int n_sessions = 0;
+    unsigned int n_clients = 0;
+    struct Session *session_list[16];
+    struct Client *client_list[64];
+
     listen(sockfd, 5);
     clilen = sizeof(cli_addr);
 
@@ -71,7 +90,18 @@ int main(int argc, char *argv[]) {
 
         // Create a new thread for this client
         pthread_t tid;
-        if (pthread_create(&tid, NULL, client_handler, (void *)new_sock) < 0) {
+
+        struct thread_args *args = malloc(sizeof(struct thread_args));
+        if (args == NULL) {
+            perror("ERROR allocating thread arguments");
+        }
+        args->new_sock = new_sock;
+        args->client_list = &client_list;
+        args->session_list = &session_list;
+        args->n_clients = &n_clients;
+        args->n_sessions = &n_sessions;
+
+        if (pthread_create(&tid, NULL, client_handler, (void *)args) < 0) {
             perror("ERROR creating thread");
             close(newsockfd);
             free(new_sock);
